fae39646089ce8669544be7db91855ae9030d44f
GabrielFM97 <gabrielfernandez090997@gmail.com>
2024-01-25 20:00:32 -0300

    first commit javascript

--- M05 Fundamentos Javascript/ejercicios/20.js
+++ M05 Fundamentos Javascript/ejercicios/20.js
@@ -3,6 +3,13 @@ function esVocal(letra) {
   // Si el usuario ingresa un string de más de un caracter, debes retornar el mensaje: "Dato incorrecto".
   // Si no es vocal, tambien debe retornar "Dato incorrecto".
   // Tu código:
+  if (letra == "a") return "Es vocal";
+  if (letra == "e") return "Es vocal";
+  if (letra == "i") return "Es vocal";
+  if (letra == "o") return "Es vocal";
+  if (letra == "u") return "Es vocal";
+  if (letra.length > 1) return "Dato incorrecto";
+  else return "Dato incorrecto";
 }
 
 module.exports = esVocal;
